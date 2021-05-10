b0d18b2f9b0541296519ef96f58df9fd814f3e95
Thomas Andrillon <31892797+andrillon@users.noreply.github.com>
2021-05-10 10:54:47 +0200

    detection of SW and analysis

--- LocalSleep/CTET_ADHD_analyse_LocalSleep_v1.m
+++ LocalSleep/CTET_ADHD_analyse_LocalSleep_v1.m
@@ -15,8 +15,10 @@ cfg.layout = 'biosemi64.lay';
 cfg.center      = 'yes';
 layout=ft_prepare_layout(cfg);
 
+ChanLabels=layout.label(1:64);
 %%
 all_slowWaves=[];
+nFc=0;
 for nF=1:length(files)
     file_name = files(nF).name;
     folder_name = files(nF).folder;
@@ -34,11 +36,31 @@ for nF=1:length(files)
     hdr=ft_read_header([folder_name filesep file_name]);
     
     load([data_path filesep 'Preproc' filesep 'CIcfeblock_ft_SW_' file_name(1:end-4)]); %,'slow_Waves','paramSW')
+    % 1: Subject Number
+    % 2: Block Number
+    % 3: Electrode Number
+    % 4: P2P amplitude
+    % 5: Start slow wave (sample from block onset)
+    nFc=nFc+1;
     for nBl=1:8
-        nout=hist(slow_Waves(slow_Waves(:,2)==nBl,3),1:64);
+        these_SWelectrodes=slow_Waves(slow_Waves(:,2)==nBl,3);
+        nout=hist(these_SWelectrodes,1:64);
         sub_table_behav=table_behav(table_behav.BlockN==nBl,:);
         duration_block=(sub_table_behav.Sample(end)-sub_table_behav.Sample(1))/hdr.Fs/60;
         densSW=nout/duration_block;
-        all_slowWaves=[all_slowWaves ; [nF nBl densSW]];
+        all_slowWaves(nFc,nBl,:)=densSW;
     end
 end
+
+%%
+% 1: Split between ADHD and Controls
+
+% 2: For Electrode Cz (48), plot the average SW density across blocks
+% (simpleTplot or RainCloudPlot) squeeze(all_slowWaves(:,:,match_str(ChanLabels,'Cz')))
+
+% 2b: Do the same thing for ADHD and controls separately
+
+% 3: Plot scalp topographies of average SW density (simpleTopoPlot_ft)
+% squeeze(mean(mean(all_slowWaves,1),2))
+
+% 3b: Do the same for ADHD and controls separately
